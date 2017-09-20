56400ee742941338df3870b90b8d5bb1cbfe7e35
Martin Hochel <hochelmartin@gmail.com>
2017-09-20 12:02:32 +0200

    feat(skate-wc): ise esnext skate and configure webpack

--- 03-wc-skate/webpack.config.js
+++ 03-wc-skate/webpack.config.js
@@ -1,4 +1,12 @@
 const { resolve, join } = require('path')
+const { readFileSync } = require('fs')
+
+const pathIsInside = require('path-is-inside')
+const findRoot = require('find-root')
+
+const PROPKEY_ESNEXT = 'esnext'
+const dir_ts = resolve(__dirname, '.')
+const dir_node_modules = resolve(__dirname, 'node_modules')
 
 module.exports = () => {
   return {
@@ -9,6 +17,8 @@ module.exports = () => {
     },
     resolve: {
       extensions: ['.js', '.ts', '.tsx'],
+      // Help webpack find `PROPKEY_ESNEXT` source code
+      mainFields: [PROPKEY_ESNEXT, 'browser', 'module', 'main'],
     },
     devtool: 'source-map',
     devServer: {
@@ -19,7 +29,11 @@ module.exports = () => {
       rules: [
         {
           test: /\.tsx?$/,
-          include: /src/,
+          include: filepath => {
+            return (
+              pathIsInside(filepath, dir_ts) || (pathIsInside(filepath, dir_node_modules) && hasPkgEsnext(filepath))
+            )
+          },
           use: [
             {
               loader: 'awesome-typescript-loader',
@@ -29,4 +43,16 @@ module.exports = () => {
       ],
     },
   }
+}
+
+/**
+ * Find package.json for file at `filepath`.
+ * Return `true` if it has a property whose key is `PROPKEY_ESNEXT`.
+ */
+function hasPkgEsnext(filepath) {
+  const pkgRoot = findRoot(filepath)
+  const packageJsonPath = resolve(pkgRoot, 'package.json')
+  const packageJsonText = readFileSync(packageJsonPath, { encoding: 'utf-8' })
+  const packageJson = JSON.parse(packageJsonText)
+  return {}.hasOwnProperty.call(packageJson, PROPKEY_ESNEXT) // (A)
 }
