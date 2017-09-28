0f56020a8e6a7aff7b3f23859ca92b71e2eef611
Martin Hochel <hochelmartin@gmail.com>
2017-09-28 20:44:04 +0200

    feat(skate-wc): add renderers examples -> preact

---
+++ 03-wc-skate/src/preact-renderer/base.ts
@@ -0,0 +1,5 @@
+import { props, withComponent } from 'skatejs'
+import withPreact from '@skatejs/renderer-preact'
+import Preact from 'preact'
+
+export const Component = withComponent(withPreact())
